b0e98e5d617b0f8da2daf100aabcd95491992eb2
GCINILE99 <gcinilecordecia@gmail.com>
2022-07-04 20:44:22 +0200

    new

--- transport_Fee.js
+++ transport_Fee.js
@@ -1,7 +1,18 @@
-function transportFee (shift) {
-if (shift== "morning") 
-  return "R20"
-else if (shift == "afternoon")
-  return "R10"
-return "free"
- }
+function transportFee(shift) {
+  if (!shift) {
+    return "";
+  }
+
+  if (shift.startsWith("morning")) {
+    return "R20";
+  }
+  else if (shift.startsWith("afternoon"))  {
+    return "R10";
+  }
+  else if (shift.startsWith("night")) {
+    return "FREE";
+  }
+  else {
+    return "N/A";
+  }
+}
